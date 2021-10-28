29df54b6de18a45d1a6dc9376d0a6c65044b148a
rymoa <rymoa.ayadich@gmail.com>
2021-10-28 22:00:55 +0900

    202110282200

--- variables.tf
+++ variables.tf
@@ -33,31 +33,6 @@ variable SQLAdmin {
   default     = "rymoasqladmin"
   description = "SqlAdminUser"
 }
-variable SQLAdminPass {
-  type        = string
-  default     = "ZAQ!2wsxcde3"
-  description = "SqlAdminPass"
-}
-variable AADAdminName {
-  type        = string
-  default     = "opadmin"
-  description = "SqlAADAdminName"
-}
-variable TenantId {
-  type        = string
-  default     = "ce2cbc43-bbef-404f-9d5b-a805d131a78f"
-  description = "tanantid"
-}
-variable ObjectId {
-  type        = string
-  default     = "7ced8772-bc22-439b-b0e6-b7b0a12864b9"
-  description = "SqlAADAdminId"
-}
-variable Sql_Security_Alert {
-  type        = string
-  default     = "rymoa.ayadich@gmail.com"
-  description = "mail"
-}
 variable DnsServiceIp {
   type        = string
   default     = "10.0.0.10"
@@ -73,3 +48,8 @@ variable DockerBridgeCidr {
   default     = "172.17.0.1/16"
   description = "AksDockerBridgeCidr"
 }
+variable SQLAdminPass {}
+variable AADAdminName {}
+variable TenantId {}
+variable ObjectId {}
+variable Sql_Security_Alert {}
