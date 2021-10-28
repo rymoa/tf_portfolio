c4e971429968f426c6f44bd29dd37f62695db711
rymoa <rymoa.ayadich@gmail.com>
2021-10-28 21:30:28 +0900

    202110282130

--- variables.tf
+++ variables.tf
@@ -1,5 +1,20 @@
-variable "Secret" {}
-variable "Param" {}
-variable "SQL" {}
-variable "AKS" {}
-variable "Alert" {}
+variable "Secret" {
+    type = "map"
+    default {}
+}
+variable "Param" {
+    type = "map"
+    default {}
+}
+variable "SQL" {
+    type = "map"
+    default {}
+}
+variable "AKS" {
+    type = "map"
+    default {}
+}
+variable "Alert" {
+    type = "map"
+    default {}
+}
